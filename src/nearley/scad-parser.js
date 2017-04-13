5c6aeacdf8715cf8faa66fe9e5540ae9eed1b4a0
Harry Hornbacher <harry@harry-hornbacher.de>
2017-04-13 19:45:57 +0200

    added tokens, reimplementing grammar

--- src/nearley/scad-parser.js
+++ src/nearley/scad-parser.js
@@ -4,81 +4,50 @@
 function id(x) {return x[0]; }
  
     require('../ast');
+
+	const tok_module = {literal: "module"},
+		tok_function = {literal: "function"};
+
+	const tok_true = {literal: "true"},
+		tok_false = {literal: "false"};
+
+	const tok_include = {literal: "include"},
+		tok_use = {literal: "use"};
+
+	const tok_le = {literal: "<="},
+		tok_ge = {literal: ">="},
+		tok_eq = {literal: "=="};
 var grammar = {
     ParserRules: [
-    {"name": "EndOfStatement", "symbols": ["_", {"literal":";"}, "_"]},
-    {"name": "_$ebnf$1", "symbols": ["WhiteSpace"], "postprocess": id},
-    {"name": "_$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "_", "symbols": ["_$ebnf$1"]},
-    {"name": "__", "symbols": ["WhiteSpace"]},
-    {"name": "WhiteSpace$ebnf$1", "symbols": [/[\r\n ]/]},
-    {"name": "WhiteSpace$ebnf$1", "symbols": ["WhiteSpace$ebnf$1", /[\r\n ]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "WhiteSpace", "symbols": ["WhiteSpace$ebnf$1"], "postprocess": d => null},
-    {"name": "ValueNode", "symbols": ["NumberValue"], "postprocess": d => d[0]},
-    {"name": "ValueNode", "symbols": ["VectorValue"], "postprocess": d => d[0]},
-    {"name": "ValueNode", "symbols": ["RangeValue"], "postprocess": d => d[0]},
-    {"name": "ValueNode", "symbols": ["StringValue"], "postprocess": d => d[0]},
-    {"name": "ValueNode", "symbols": ["ReferenceValue"], "postprocess": d => d[0]},
-    {"name": "StringValue$ebnf$1", "symbols": [/[^"]/]},
-    {"name": "StringValue$ebnf$1", "symbols": ["StringValue$ebnf$1", /[^"]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "StringValue", "symbols": [{"literal":"\""}, "StringValue$ebnf$1", {"literal":"\""}], "postprocess": d => new StringValue(_.flattenDeep(d[1]).join(''))},
-    {"name": "ReferenceValue", "symbols": ["Name"], "postprocess":  d => {
-        	if(d[0] === 'true')
-        		return new BooleanValue(true);
-        	else if(d[0] === 'false')
-        		return new BooleanValue(false);
-        	else
-        		return new ReferenceValue(d[0], false);
-        } },
-    {"name": "ReferenceValue", "symbols": [{"literal":"-"}, "_", "Name"], "postprocess": d => new ReferenceValue(d[2], true)},
-    {"name": "RangeValue", "symbols": [{"literal":"["}, "_", "ValueNode", "_", {"literal":":"}, "_", "ValueNode", "_", {"literal":"]"}], "postprocess": d => new RangeValue(d[2], d[6])},
-    {"name": "RangeValue", "symbols": [{"literal":"["}, "_", "ValueNode", "_", {"literal":":"}, "_", "ValueNode", "_", {"literal":":"}, "_", "ValueNode", "_", {"literal":"]"}], "postprocess": d => new RangeValue(d[2], d[10], d[6])},
-    {"name": "VectorValue", "symbols": [{"literal":"["}, "_", "VectorList", "_", {"literal":"]"}], "postprocess": d => new VectorValue(d[2], false)},
-    {"name": "VectorValue", "symbols": [{"literal":"-"}, "_", {"literal":"["}, "_", "VectorList", "_", {"literal":"]"}], "postprocess": d => new VectorValue(d[4], true)},
-    {"name": "VectorList", "symbols": ["ValueNode"], "postprocess": d => d[0]},
-    {"name": "VectorList", "symbols": ["VectorList", "_", {"literal":","}, "_", "ValueNode"], "postprocess": d => _.flattenDeep([d[0], d[4]])},
-    {"name": "NumberValue", "symbols": ["Float"], "postprocess": d => new NumberValue(parseFloat(d[0]), false)},
-    {"name": "NumberValue", "symbols": ["Integer"], "postprocess": d => new NumberValue(parseFloat(d[0]), false)},
-    {"name": "NumberValue", "symbols": [{"literal":"-"}, "_", "Float"], "postprocess": d => new NumberValue(parseFloat(d[2]), true)},
-    {"name": "NumberValue", "symbols": [{"literal":"-"}, "_", "Integer"], "postprocess": d => new NumberValue(parseFloat(d[2]), true)},
-    {"name": "Float", "symbols": ["Integer", {"literal":"."}, "Integer"], "postprocess": d => d[0] + d[1] + d[2]},
-    {"name": "Integer", "symbols": [/[0-9]/], "postprocess": d => d[0]},
-    {"name": "Integer", "symbols": ["Integer", /[0-9]/], "postprocess": d => d[0] + d[1]},
-    {"name": "RootNode$ebnf$1", "symbols": []},
-    {"name": "RootNode$ebnf$1$subexpression$1", "symbols": ["StatementNode"]},
-    {"name": "RootNode$ebnf$1", "symbols": ["RootNode$ebnf$1", "RootNode$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "RootNode", "symbols": ["RootNode$ebnf$1"], "postprocess": d => new RootNode(_.flattenDeep(d))},
-    {"name": "BlockNode$ebnf$1", "symbols": []},
-    {"name": "BlockNode$ebnf$1$subexpression$1", "symbols": ["StatementNode"]},
-    {"name": "BlockNode$ebnf$1", "symbols": ["BlockNode$ebnf$1", "BlockNode$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "BlockNode", "symbols": [{"literal":"{"}, "_", "BlockNode$ebnf$1", "_", {"literal":"}"}], "postprocess": d => new BlockNode(_.flattenDeep(d[2]))},
-    {"name": "StatementNode", "symbols": ["VariableNode", "EndOfStatement"], "postprocess": d => d[0]},
-    {"name": "StatementNode", "symbols": ["IncludeNode", "EndOfStatement"], "postprocess": d => d[0]},
-    {"name": "StatementNode", "symbols": ["Comment", "_"], "postprocess": d => d[0]},
-    {"name": "VariableNode", "symbols": ["Name", "_", {"literal":"="}, "_", "TermNode"], "postprocess": d => new VariableNode(d[0], d[4])},
-    {"name": "VariableNode", "symbols": ["Name", "_", {"literal":"="}, "_", "ValueNode"], "postprocess": d => new VariableNode(d[0], d[4])},
-    {"name": "IncludeNode$string$1", "symbols": [{"literal":"i"}, {"literal":"n"}, {"literal":"c"}, {"literal":"l"}, {"literal":"u"}, {"literal":"d"}, {"literal":"e"}, {"literal":" "}, {"literal":"<"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "IncludeNode", "symbols": ["IncludeNode$string$1", "Path", {"literal":">"}], "postprocess": d => new IncludeNode(d[1])},
-    {"name": "IncludeNode$string$2", "symbols": [{"literal":"u"}, {"literal":"s"}, {"literal":"e"}, {"literal":" "}, {"literal":"<"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "IncludeNode", "symbols": ["IncludeNode$string$2", "Path", {"literal":">"}], "postprocess": d => new UseNode(d[1])},
-    {"name": "TermNode", "symbols": [{"literal":"("}, "_", "TermNode", "_", {"literal":")"}], "postprocess": d => d[2]},
-    {"name": "TermNode", "symbols": ["ValueNode", "Operator", "ValueNode"], "postprocess": d => new TermNode([d[0],d[2]], d[1])},
-    {"name": "TermNode", "symbols": ["TermNode", "Operator", "TermNode"], "postprocess": d => new TermNode([d[0],d[2]], d[1])},
-    {"name": "Operator", "symbols": ["_", /[-+*\/]/, "_"], "postprocess": d => d[1]},
-    {"name": "Path$ebnf$1", "symbols": [/[^>]/]},
-    {"name": "Path$ebnf$1", "symbols": ["Path$ebnf$1", /[^>]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "Path", "symbols": ["Path$ebnf$1"], "postprocess": d => d[0].join('')},
-    {"name": "Name", "symbols": [/[A-Za-z_$]/], "postprocess": d => d[0]},
-    {"name": "Name", "symbols": ["Name", /[A-Za-z0-9_]/], "postprocess": d => d[0] + d[1]},
-    {"name": "Comment$string$1", "symbols": [{"literal":"/"}, {"literal":"/"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Comment$ebnf$1", "symbols": [/[^\n]/]},
-    {"name": "Comment$ebnf$1", "symbols": ["Comment$ebnf$1", /[^\n]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "Comment", "symbols": ["Comment$string$1", "Comment$ebnf$1"], "postprocess": d => new CommentNode(d[1].join('').trim())},
-    {"name": "Comment$string$2", "symbols": [{"literal":"/"}, {"literal":"*"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Comment$ebnf$2", "symbols": [/[^*\/]/]},
-    {"name": "Comment$ebnf$2", "symbols": ["Comment$ebnf$2", /[^*\/]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "Comment$string$3", "symbols": [{"literal":"*"}, {"literal":"/"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Comment", "symbols": ["Comment$string$2", "Comment$ebnf$2", "Comment$string$3"], "postprocess": d => new CommentNode(d[1].join(''), true)}
+    {"name": "_$ebnf$1", "symbols": []},
+    {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": function(d) {return null;}},
+    {"name": "__$ebnf$1", "symbols": ["wschar"]},
+    {"name": "__$ebnf$1", "symbols": ["__$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "__", "symbols": ["__$ebnf$1"], "postprocess": function(d) {return null;}},
+    {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id},
+    {"name": "RootNode", "symbols": ["Body"], "postprocess": id},
+    {"name": "Body", "symbols": []},
+    {"name": "Body", "symbols": ["Statement", "Body"]},
+    {"name": "Statement", "symbols": [{"literal":";"}]},
+    {"name": "Statement", "symbols": [{"literal":"{"}, "Body", {"literal":"}"}]},
+    {"name": "Statement", "symbols": ["Identifier", "_", {"literal":"="}, "_", "Expression", "_", {"literal":";"}]},
+    {"name": "Statement", "symbols": [tok_module, "__", "Identifier", "_", {"literal":"("}, "_", "Parameters", "_", {"literal":")"}, "_", "Statement", "__"]},
+    {"name": "Statement", "symbols": [tok_function, "__", "Identifier", "_", {"literal":"("}, "_", "Parameters", "_", {"literal":")"}, "_", {"literal":"="}, "_", "Expression", "__"]},
+    {"name": "Expression", "symbols": [tok_true], "postprocess": id},
+    {"name": "Expression", "symbols": [tok_false], "postprocess": id},
+    {"name": "Parameters", "symbols": []},
+    {"name": "Parameters", "symbols": ["Parameter"]},
+    {"name": "Parameters", "symbols": ["Parameters", "_", {"literal":","}, "_", "Parameter"]},
+    {"name": "Parameter", "symbols": ["Identifier"]},
+    {"name": "Parameter", "symbols": ["Identifier", "_", {"literal":"="}, "_", "Expression"]},
+    {"name": "Arguments", "symbols": []},
+    {"name": "Arguments", "symbols": ["Argument"]},
+    {"name": "Arguments", "symbols": ["Arguments", "_", {"literal":","}, "_", "Argument"]},
+    {"name": "Argument", "symbols": ["Expression"]},
+    {"name": "Argument", "symbols": ["Identifier", "_", {"literal":"="}, "_", "Expression"]},
+    {"name": "Identifier", "symbols": [/[A-Za-z_$]/], "postprocess": d => d[0]},
+    {"name": "Identifier", "symbols": ["Identifier", /[A-Za-z0-9_]/], "postprocess": d => d[0] + d[1]}
 ]
   , ParserStart: "RootNode"
 }
--- src/nearley/scad-parser.ne
+++ src/nearley/scad-parser.ne
@@ -1,41 +1,90 @@
 @{% 
     require('../ast');
+
+	const tok_module = {literal: "module"},
+		tok_function = {literal: "function"};
+
+	const tok_true = {literal: "true"},
+		tok_false = {literal: "false"};
+
+	const tok_include = {literal: "include"},
+		tok_use = {literal: "use"};
+
+	const tok_le = {literal: "<="},
+		tok_ge = {literal: ">="},
+		tok_eq = {literal: "=="};
 %}
  
-@include "./misc.ne"
-@include "./values.ne"
+@builtin "whitespace.ne"
+#@include "./misc.ne"
+#@include "./values.ne"  
 
-RootNode -> (StatementNode):* {% d => new RootNode(_.flattenDeep(d)) %}
+RootNode -> Body {% id %}
 
-BlockNode -> "{" _ (StatementNode):* _ "}" {% d => new BlockNode(_.flattenDeep(d[2])) %}
+Body ->
+	null
+	| Statement Body
 
-StatementNode ->
-	VariableNode EndOfStatement  {% d => d[0] %}
-	| IncludeNode EndOfStatement {% d => d[0] %}
-	| Comment _  {% d => d[0] %}
+Statement ->
+	";"
+	| "{" Body "}"
+	| Identifier _ "=" _ Expression _ ";"
+	| %tok_module __ Identifier _ "(" _ Parameters _ ")" _ Statement __
+	| %tok_function __ Identifier _ "(" _ Parameters _ ")" _ "=" _ Expression __
 
-VariableNode -> 
-	Name _  "=" _ TermNode {% d => new VariableNode(d[0], d[4]) %}
-	| Name _  "=" _ ValueNode {% d => new VariableNode(d[0], d[4]) %}
+Expression ->
+	%tok_true {% id %}
+	| %tok_false {% id %}
 
-IncludeNode ->
-	"include <" Path ">"  {% d => new IncludeNode(d[1]) %}
-	| "use <" Path ">"  {% d => new UseNode(d[1]) %}
+Parameters ->
+	null
+	| Parameter
+	| Parameters _ "," _ Parameter
 
-TermNode -> 
-	"(" _ TermNode _ ")"  {% d => d[2] %}
-	| ValueNode Operator ValueNode {% d => new TermNode([d[0],d[2]], d[1]) %}
-	| TermNode Operator TermNode {% d => new TermNode([d[0],d[2]], d[1]) %}
+Parameter ->
+	Identifier
+	| Identifier _ "=" _ Expression
 
-Operator -> _ [-+*/] _   {% d => d[1] %}
+Arguments ->
+	null
+	| Argument
+	| Arguments _ "," _ Argument
 
-Path ->
-	[^>]:+ {% d => d[0].join('') %}
-	
-Name ->
+Argument ->
+	Expression
+	| Identifier _ "=" _ Expression
+
+Identifier ->
 	[A-Za-z_$] {% d => d[0] %}
-	| Name [A-Za-z0-9_] {% d => d[0] + d[1] %}
- 
-Comment ->
-	"//" [^\n]:+ {% d => new CommentNode(d[1].join('').trim()) %}
-	| "/*" [^*/]:+ "*/" {% d => new CommentNode(d[1].join(''), true) %}
+	| Identifier [A-Za-z0-9_] {% d => d[0] + d[1] %}
+
+#BlockNode -> "{" _ (StatementNode):* _ "}" {% d => new BlockNode(_.flattenDeep(d[2])) %}
+
+#StatementNode ->
+#	VariableNode EndOfStatement  {% d => d[0] %}
+#	| IncludeNode EndOfStatement {% d => d[0] %}
+#	| Comment _  {% d => d[0] %}
+
+#VariableNode -> 
+#	Name _  "=" _ TermNode {% d => new VariableNode(d[0], d[4]) %}
+#	| Name _  "=" _ ValueNode {% d => new VariableNode(d[0], d[4]) %}
+
+#IncludeNode ->
+#	%tok_include _ "<" Path ">"  {% d => new IncludeNode(d[1]) %}
+#	| %tok_use _ "<" Path ">"  {% d => new UseNode(d[1]) %}
+
+#TermNode -> 
+#	"(" _ TermNode _ ")"  {% d => d[2] %}
+#	| ValueNode Operator ValueNode {% d => new TermNode([d[0],d[2]], d[1]) %}
+#	| TermNode Operator TermNode {% d => new TermNode([d[0],d[2]], d[1]) %}
+
+#Expression ->
+#	"true"
+#	| "false"
+#	| "undef"
+
+#Operator -> _ [-+*/] _   {% d => d[1] %}
+
+#Path ->
+#	[^>]:+ {% d => d[0].join('') %}
+
