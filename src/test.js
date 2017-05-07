4828bd8da9327d1bbc5abc29ae47a6bc7454cdd0
hhornbacher <harry@harry-hornbacher.de>
2017-05-08 00:15:50 +0200

    rewriting all tests systematically

--- src/test.js
+++ src/test.js
@@ -1,174 +1,227 @@
-const expect = require('expect.js');
+const expect = require('expect.js'),
+  Random = require('random-js');
 
 const SCADParser = require('../src');
 
-let parser = new SCADParser();
-describe('SCADParser', function () {
+const parser = new SCADParser();
 
+const randEngine = Random.engines.nativeMath;
+
+
+const miscExpressions = [
+  'a*b',
+  '4.77/c',
+  '1+(2-(3+(4-5)+6)-7)',
+  '( -a * z) +3'
+];
+
+const miscStatements = [
+  'color("blue") cube([1,2,3]);',
+  'action(a,b,c);',
+  'variable=ref*3+x*x;'
+];
+
+const randMaps = {
+  operators: '*/%+-'
+};
+
+const rand = {
+  pick: (values) => Random.pick(randEngine, _.isString(values) ? values.split('') : values),
+  string: (value, length) => Random.string(value)(randEngine, length),
+  integer: (min, max) => Random.integer(min, max)(randEngine),
+  float: (min, max, inclusive) => Random.real(min, max, inclusive)(randEngine),
+  identifier: () => {
+    const prefix = rand.pick('_$abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    const name = rand.string('_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', rand.integer(3, 9))
+    return prefix + name;
+  },
+  statement: (count = null) => {
+    if (!_.isNumber(count))
+      return rand.pick(miscStatements);
+    return _.times(count, () => rand.statement());
+  },
+  value: () => {
+    let value = 1;
+    switch(rand.integer(0,100)%3) {
+      case 0:
+      value=rand.integer(-10e6, 10e6);
+      break;
+      case 1:
+      value=rand.float(-10e6, 10e6, true);
+      break;
+      case 2:
+      value=rand.float(-10e6, 10e6, true).toExponential();
+      break;
+    }
+    return value;
+  },
+  expression: () => {
+    let length = rand.integer(2,6);
+    let values = _.times(length, () => rand.value());
+    let operators = _.times(length-1, () => rand.pick(randMaps.operators));
+
+    return _.map(values, (value, index) => {
+      return `${value}${operators[index] ? operators[index] : ''}`;
+    }).join('');
+  }
+};
+
+describe('SCADParser', function () {
 
   it('should have created an instance of SCADParser', function () {
     expect(parser).to.be.a(SCADParser);
   });
 
-  describe('Parse variables', function () {
-    it('should parse number', function () {
-      const root1 = parser.parseAST('number01.scad',
-        'myNumber =1;'
-      );
-      expect(root1.children[0]).to.be.a(VariableNode);
-      expect(root1.children[0].value.isEqual(new NumberValue([], '1'))).to.equal(true);
-      const root2 = parser.parseAST('number02.scad',
-        'myNumber=  -1.78;'
-      );
-      expect(root2.children[0]).to.be.a(VariableNode);
-      expect(root2.children[0].value.isEqual(new NumberValue([], '1.78', true).setNegative(true))).to.equal(true);
-      const root3 = parser.parseAST('number03.scad',
-        'myNumber=1.78e86\n;'
-      );
-      expect(root3.children[0]).to.be.a(VariableNode);
-      expect(root3.children[0].value.isEqual(new NumberValue([], '1.78e86'))).to.equal(true);
-      expect(root3.children[0].value.isEqual(new NumberValue([], '23'))).to.equal(false);
+  describe('parseAST', function () {
+    const statementTest = (statement, file, expected, furtherTests = (root) => { }) => {
+
+      function truncateStatement(statement = '', max = 64) {
+        const placeholder = '...';
+        const statementLength = statement.length;
+        if (statementLength > max) {
+          const partA = statement.substring(0, max - placeholder.length - 1);
+          return `${partA}${placeholder}`;
+        }
+        return statement;
+      }
+
+      it(`should parse ${/^[aeiou].*/i.test(expected.name) ? 'an' : 'a'} ${expected.name}: ${JSON.stringify(truncateStatement(statement))}`, function () {
+        const root = parser.parseAST(file, statement);
+        expect(root.children).to.have.length(1);
+        expect(root.children[0]).to.be.a(expected);
+        furtherTests(root);
+      });
+    };
+
+    describe('with variable statements as input', function () {
+      const variableTest = (value = 1, varType = NumberValue, furtherTests = (value) => { }) => {
+        const name = rand.identifier();
+        const statement = `${name} = ${value};`;
+        const file = `var-${name}-${varType.name}.scad`;
+        statementTest(statement, file, VariableNode, root => {
+          expect(root.children[0].value).to.be.a(varType);
+
+          if (
+            varType !== VectorValue && 
+            varType !== RangeValue && 
+            varType !== ExpressionNode
+            ) {
+            let cleanValue = value.toString().replace('-', '').replace(/"([^"]*)"/, '$1');
+            if (varType === BooleanValue)
+              cleanValue = cleanValue === 'true' ? true : false;
+            const expectedValue = new varType([], cleanValue);
+            if (value < 0)
+              expectedValue.setNegative(true);
+            expect(root.children[0].value.isEqual(expectedValue)).to.equal(true);
+          }
+
+          furtherTests(root.children[0].value);
+        });
+      };
+
+      for (let i = 0; i < 5; i++) {
+        variableTest(rand.integer(-10e6, 10e6), NumberValue);
+      }
+      for (let i = 0; i < 5; i++) {
+        variableTest(rand.float(-10e6, 10e6, true), NumberValue);
+      }
+      for (let i = 0; i < 5; i++) {
+        variableTest(rand.float(-10e6, 10e6, true).toExponential(), NumberValue);
+      }
+
+      for (let i = 0; i < 5; i++) {
+        variableTest('"' + rand.string('_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', rand.integer(3, 30)) + '"', StringValue);
+      }
+
+      variableTest(true, BooleanValue);
+      variableTest(false, BooleanValue);
+
+      for (let i = 0; i < 5; i++) {
+        const vectorValues = _.times(rand.integer(3, 30), () => rand.integer(-10e6, 10e6));
+        const vector = `[${vectorValues.join(', ')}]`;
+        variableTest(vector, VectorValue, (value) => {
+          expect(value.value).to.have.length(vectorValues.length);
+          let expected = new VectorValue([], _.map(vectorValues, (expected, index) => {
+            const expectedValue = new NumberValue([], expected.toString().replace('-', ''));
+            if (expected < 0)
+              expectedValue.setNegative(true);
+            return expectedValue;
+          }));
+          expect(value.isEqual(expected)).to.equal(true);
+        });
+      }
+
+      for (let i = 0; i < 5; i++) {
+        const range = `[${_.times(rand.integer(2, 3), () => rand.integer(-10e6, 10e6)).join(':')}]`;
+        variableTest(range, RangeValue, (value) => {
+          /** @todo Check values */
+        });
+      }
+
+      for (let i = 0; i < 5; i++) {
+        variableTest(rand.identifier(), ReferenceValue);
+      }
+
+      for (let i = 0; i < 5; i++) {
+        variableTest(rand.expression(), ExpressionNode);
+      }
     });
 
-    it('should parse string', function () {
-      const root = parser.parseAST('string.scad',
-        'myStr ="a string";'
-      );
-      expect(root.children[0]).to.be.a(VariableNode);
-      expect(root.children[0].value.isEqual(new StringValue([], 'a string'))).to.equal(true);
-      expect(root.children[0].value.isEqual(new StringValue([], 'just wrong'))).to.equal(false);
+    describe('with comment statements as input', function () {
+      let slCommentStatement = '// Single line comment\n';
+      let mlCommentStatement = '/* Multi\nline\ncomment */';
+
+      statementTest(slCommentStatement, 'sl-comment.scad', CommentNode, root => {
+        expect(root.children[0].text).to.equal('Single line comment');
+        expect(root.children[0].multiline).to.equal(false);
+      });
+      statementTest(mlCommentStatement, 'ml-comment.scad', CommentNode, root => {
+        expect(root.children[0].text).to.equal(' Multi\nline\ncomment ');
+        expect(root.children[0].multiline).to.equal(true);
+      });
     });
 
-    it('should parse boolean', function () {
-      const root = parser.parseAST('boolean.scad',
-        'myBool =false;'
-      );
-      expect(root.children[0]).to.be.a(VariableNode);
-      expect(root.children[0].value.isEqual(new BooleanValue([], false))).to.equal(true);
-      expect(root.children[0].value.isEqual(new BooleanValue([], true))).to.equal(false);
+    describe('with include/use statements as input', function () {
+      let includeStatement = 'include <file.scad>;';
+      let useStatement = 'use <file.scad>;';
+
+      statementTest(includeStatement, 'include.scad', IncludeNode, root => {
+        expect(root.children[0].file).to.equal('file.scad');
+      });
+      statementTest(useStatement, 'use.scad', UseNode, root => {
+        expect(root.children[0].file).to.equal('file.scad');
+      });
     });
 
-    it('should parse vector', function () {
-      const root = parser.parseAST('vector.scad',
-        'myVector = [1,[2,3]];'
-      );
-      expect(root.children[0]).to.be.a(VariableNode);
-      expect(root.children[0].value.isEqual(
-        new VectorValue([], [
-          new NumberValue([], '1'),
-          new VectorValue([], [
-            new NumberValue([], '2'),
-            new NumberValue([], '3')
-          ])
-        ])
-      )).to.equal(true);
-      expect(root.children[0].value.isEqual(
-        new VectorValue([], [
-          new NumberValue([], '1'),
-          new NumberValue([], '2'),
-          new NumberValue([], '3')
-        ])
-      )).to.equal(false);
+    describe('with module statements as input', function () {
+      const moduleTest = (statementCount) => {
+        const modStatements = rand.statement(statementCount);
+        let moduleStatement = `module ${rand.identifier()}() { ${modStatements.join('\n')} }`;
+        statementTest(moduleStatement, 'module.scad', ModuleNode, root => {
+          expect(root.children[0].children).to.have.length(modStatements.length);
+        });
+      };
+
+      for (let i = 0; i < 5; i++) {
+        moduleTest(rand.integer(4, 8));
+      }
     });
 
-    it('should parse range', function () {
-      const root1 = parser.parseAST('range01.scad',
-        'myRange = [1:10];'
-      );
-      expect(root1.children[0]).to.be.a(VariableNode);
-      expect(root1.children[0].value.isEqual(
-        new RangeValue([],
-          new NumberValue([], '1'), new NumberValue([], '10')
-        )
-      )).to.equal(true);
-      const root2 = parser.parseAST('range02.scad',
-        'myRange = [1:0.5:10];'
-      );
-      expect(root2.children[0]).to.be.a(VariableNode);
-      expect(root2.children[0].value.isEqual(
-        new RangeValue([],
-          new NumberValue([], '1'), new NumberValue([], '10'), new NumberValue([], '0.5')
-        )
-      )).to.equal(true);
-    });
+    describe('with function statements as input', function () {
+      const functionTest = () => {
+        const funcExpression = rand.expression();
+        let functionStatement = `function ${rand.identifier()}() = ${funcExpression};`;
+        statementTest(functionStatement, 'module.scad', FunctionNode, root => {
+        });
+      };
 
-    it('should parse reference', function () {
-      const root1 = parser.parseAST('reference01.scad',
-        'myRef = ref;'
-      );
-      expect(root1.children[0]).to.be.a(VariableNode);
-      expect(root1.children[0].value.isEqual(new ReferenceValue([], 'ref'))).to.equal(true);
-      expect(root1.children[0].value.isEqual(new ReferenceValue([], 'ref').setNegative(true))).to.equal(false);
-      const root2 = parser.parseAST('reference02.scad',
-        'myRef = -ref;'
-      );
-      expect(root2.children[0]).to.be.a(VariableNode);
-      expect(root2.children[0].value.isEqual(new ReferenceValue([], 'ref').setNegative(true))).to.equal(true);
-      expect(root2.children[0].value.isEqual(new ReferenceValue([], 'ref'))).to.equal(false);
-    });
-
-    it('should parse expression', function () {
-      const root = parser.parseAST('vector.scad',
-        'myVector = 5*7+6;'
-      );
-      expect(root.children[0]).to.be.a(VariableNode);
+      for (let i = 0; i < 5; i++) {
+        functionTest();
+      }
     });
   });
 
-  describe('Parse examples', function () {
-    it('should parse example 1', function () {
-      const root = parser.parseAST('./examples/ex1.scad');
-      expect(root.children.length).to.equal(23);
-      const firstToken = parser.getToken(1, 2, './examples/ex1.scad');
-      expect(firstToken.type).to.be('identifier');
-      expect(firstToken.value).to.be('string');
-      expect(root.findByToken(firstToken).name).to.be('string');
-      expect(root.findByType('Variable').length).to.equal(2);
-      expect(root.findByType('Variable').length).to.equal(2);
-      expect(root.findByType('Module').length).to.equal(1);
-      expect(root.findByType('Function').length).to.equal(1);
-      expect(root.findByType('Action').length).to.equal(4);
-      expect(root.findByName('test').length).to.equal(2);
-      expect(root.findByName('XYZ').length).to.equal(0);
-      expect(root.findByValue(new NumberValue([], '10'))[0].name).to.be('XXX');
-      expect(root.toString()).to.contain('Root');
-    });
-    it('should parse example 2', function () {
-      const root = parser.parseAST('./examples/ex2.scad');
-      expect(root.children.length).to.equal(26);
-      expect(root.findByType('Variable').length).to.equal(1);
-      expect(root.findByType('Module').length).to.equal(1);
-      expect(root.findByType('Function').length).to.equal(0);
-      expect(root.findByType('Action').length).to.equal(4);
-      expect(root.findByName('color').length).to.equal(1);
-      expect(root.findByName('XYZ').length).to.equal(0);
-      expect(root.toString()).to.contain('Root');
-    });
-    it('should parse example 3', function () {
-      const root = parser.parseAST('./examples/ex3.scad');
-      expect(root.children.length).to.equal(29);
-      expect(root.findByType('Variable').length).to.equal(3);
-      expect(root.findByType('Module').length).to.equal(1);
-      expect(root.findByType('Function').length).to.equal(0);
-      expect(root.findByType('Action').length).to.equal(13);
-      expect(root.findByName('cube').length).to.equal(3);
-      expect(root.findByName('XYZ').length).to.equal(0);
-      expect(root.toString()).to.contain('Root');
-    });
-    it('should parse example 4', function () {
-      const root = parser.parseAST('./examples/ex4.scad');
-      expect(root.children.length).to.equal(8);
-      expect(root.findByType('Variable').length).to.equal(1);
-      expect(root.findByType('Module').length).to.equal(0);
-      expect(root.findByType('Function').length).to.equal(0);
-      expect(root.findByType('Action').length).to.equal(0);
-      expect(root.findByName('MyString').length).to.equal(1);
-      expect(root.findByName('NotValid').length).to.equal(0);
-      expect(root.toString()).to.contain('Root');
-    });
-  });
-
-  describe('Throw errors', function () {
+  describe('Exceptions', function () {
     it('should throw an error, if neither code or file are passed to parseAST', function () {
       expect(parser.parseAST).to.throwException(/You have to pass either code or file parameter!/);
     });
@@ -197,4 +250,5 @@ describe('SCADParser', function () {
       }).to.throwException(/Parser error/);
     });
   });
+
 });
