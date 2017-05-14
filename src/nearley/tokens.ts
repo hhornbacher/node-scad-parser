c6956ff102e3f9fa9051f9bc825e6c5b0d2b6f4d
hhornbacher <harry@harry-hornbacher.de>
2017-05-14 15:34:27 +0200

    fixed tokeRules export

--- src/nearley/tokens.ts
+++ src/nearley/tokens.ts
@@ -1,10 +1,10 @@
 /**
  * Prepare and initialize tokens to be used in the grammar
- * @module nearley/tokens
+ *
  */
-import * as _ from 'lodash';
+import * as _ from "lodash";
 
-export interface Token {
+interface Token {
     type: string;
     value: string;
     toString: Function;
@@ -15,7 +15,7 @@ export interface Token {
     col: number;
 }
 
-export const tokens = {
+const tokens: { [key: string]: string | Array<string> | { match: RegExp, lineBreaks: boolean } | RegExp } = {
     include: { match: /include\s*<(.+)>/, lineBreaks: true },
     use: { match: /use\s*<(.+)>/, lineBreaks: true },
     moduleDefinition: { match: /module\s*([A-Za-z_$][A-Za-z0-9_]*)\s*\(/, lineBreaks: true },
@@ -48,19 +48,23 @@ export const tokens = {
     LexerError: require('moo').error
 }
 
-export let tokenRules: any = _.each(tokens, (value, key) => {
-    if (value instanceof Array || value.value instanceof Array) {
+export type TokenRules = { [key: string]: { test: (tok: any) => boolean } };
+
+const tokenRules: TokenRules = _.transform(tokens, (rules, value, key) => {
+    if (value instanceof Array) {
         const keywords = value;
         _.each(keywords, (keyword) => {
             // add tester functions for each
-            tokenRules[key + '_' + keyword] = {
+            rules[key + '_' + keyword] = {
                 test: (tok: any) =>
                     tok.type === key && tok.value === keyword
             };
         });
     }
-
     // add tester function for `key`
-    tokenRules[key] = { test: (tok: any) => tok.type === key }
-});;
+    rules[key] = { test: (tok: any) => tok.type === key }
+}, {} as TokenRules);
 
+export {
+    Token, tokens, tokenRules
+};
